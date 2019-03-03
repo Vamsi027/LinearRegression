7aaf797dc82ec714e6bafc97f903726f6ddd3f3c
Vamsi027 <39871596+Vamsi027@users.noreply.github.com>
2019-03-03 12:30:39 +0530

    Add files via upload

---
+++ simple_linear_regression.py
@@ -0,0 +1,47 @@
+# Importing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#importing dataset
+dataset=pd.read_csv('Salary_Data.csv')
+X=dataset.iloc[:,:-1].values
+Y=dataset.iloc[:,1].values
+
+#splitting into testset and trainingset
+from sklearn.model_selection import train_test_split
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=1/3,random_state=0)
+
+#Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X=StandardScaler()
+X_train=sc_X.fit_transform(X_train)
+X_test=sc_X.transform(X_test)"""
+
+#fitting simple linear regression to training sets
+from sklearn.linear_model import LinearRegression
+regressor=LinearRegression()
+regressor.fit(X_train,Y_train)
+
+#predicting the test set
+y_pred=regressor.predict(X_test)
+
+#visualizing the training results
+plt.scatter(X_train,Y_train ,color='red')
+plt.plot(X_train,regressor.predict(X_train),color='blue')
+plt.title('Salary vs Experience(Training Set)')
+plt.xlabel('Years Of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+#visualizing the test results
+plt.scatter(X_test,Y_test ,color='red')
+plt.plot(X_train,regressor.predict(X_train),color='blue')
+plt.title('Salary vs Experience(Test Set)')
+plt.xlabel('Years Of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+#testing the goodness of fit
+from sklearn.metrics import mean_squared_error
+r2_score=regressor.score(X_train,Y_train)
