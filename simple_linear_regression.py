a2046be4b41b7db622261e19b10824793d43a8a7
Vamsi027 <39871596+Vamsi027@users.noreply.github.com>
2019-03-08 17:18:16 +0530

    Update simple_linear_regression.py

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -12,12 +12,6 @@ Y=dataset.iloc[:,1].values
 from sklearn.model_selection import train_test_split
 X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=1/3,random_state=0)
 
-#Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X=StandardScaler()
-X_train=sc_X.fit_transform(X_train)
-X_test=sc_X.transform(X_test)"""
-
 #fitting simple linear regression to training sets
 from sklearn.linear_model import LinearRegression
 regressor=LinearRegression()
