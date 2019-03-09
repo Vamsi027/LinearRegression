37365af8d6d789b69ef5b76fbfdc996dc5f5aa66
Vamsi027 <39871596+Vamsi027@users.noreply.github.com>
2019-03-09 09:11:58 +0530

    Update polynomial_regression.py

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #importing dataset
-dataset=pd.read_csv('Position_Salaries.csv')
+dataset=pd.read_csv('')#Enter the csv file
 X=dataset.iloc[:,1:2].values
 Y=dataset.iloc[:,2].values
 
@@ -18,9 +18,9 @@ linear_regressor2.fit(X_poly,Y)
 #Visualizing the dataset on Polynomial Regression
 X_grid=np.arange(min(X),max(X),0.1)
 X_grid=X_grid.reshape((len(X_grid),1))
-plt.scatter(X,Y,color='red')
-plt.plot(X_grid,linear_regressor2.predict(polynomial_regressor.fit_transform(X_grid)),color='blue')
-plt.title('Polynomial Regression')
-plt.xlabel('Position Level')
-plt.ylabel('Salary')
+plt.scatter(X,Y,color='')
+plt.plot(X_grid,linear_regressor2.predict(polynomial_regressor.fit_transform(X_grid)),color='')
+plt.title('')
+plt.xlabel('')
+plt.ylabel('')
 plt.show()
