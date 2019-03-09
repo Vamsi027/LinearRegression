6283c0f396c266afd7f078f4b1736a9ce4047af8
Vamsi027 <39871596+Vamsi027@users.noreply.github.com>
2019-03-09 09:09:50 +0530

    Update SVR.py

--- SVR.py
+++ SVR.py
@@ -4,13 +4,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #importing dataset
-dataset=pd.read_csv('Position_Salaries.csv')
+dataset=pd.read_csv('')#Enter the csv file you want to import
 X=dataset.iloc[:,1:2].values
 Y=dataset.iloc[:,-1].values
-
-#splitting into testset and trainingset
-"""from sklearn.model_selection import train_test_split
-X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,random_state=0)"""
 
 #Feature Scaling
 from sklearn.preprocessing import StandardScaler
@@ -27,24 +23,12 @@ regressor=SVR(kernel='rbf')
 regressor.fit(X,Y)
 
 #Predicting a new result
-y_pred=sc_Y.inverse_transform(regressor.predict(sc_X.transform(np.array([[6.5]]))))
+y_pred=sc_Y.inverse_transform(regressor.predict(sc_X.transform(np.array([[]))))
 
 #Visualizing SVR Results
-plt.scatter(X,Y,color='red')
-plt.plot(X,regressor.predict(X),color='blue')
-plt.title('SVR')
-plt.xlabel('Position Level')
-plt.ylabel('Salary')
+plt.scatter(X,Y,color='')
+plt.plot(X,regressor.predict(X),color='')
+plt.title('')
+plt.xlabel('')
+plt.ylabel('')
 plt.show()
-
-#Visualizing SVR results(High Resolution or Smooth Curve)
-X_grid=np.arange(min(X),max(X),0.1)
-X_grid=X_grid.reshape((len(X_grid),1))
-plt.scatter(X,Y,color='red')
-plt.plot(X_grid,regressor.predict(X_grid),color='blue')
-plt.title('SVR')
-plt.xlabel('Position Level')
-plt.ylabel('Salary')
-plt.show()
-
-
